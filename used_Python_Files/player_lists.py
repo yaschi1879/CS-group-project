c64613852a71c7169630fe5a49dc41ce5eca78a0
Yannick <yannick03schmid@gmail.com>
2024-11-17 17:23:26 +0100

    h

---
+++ used_Python_Files/player_lists.py
@@ -0,0 +1,13 @@
+club_id = [281, 418, 27, 31, 12, 583, 16, 631, 46, 1050, 15, 985, 131, 294, 720, 
+            13, 23826, 379, 800, 11, 506, 5, 610, 6195, 2282, 398, 24, 368, 124, 
+            234, 681, 383, 336, 62, 244, 1082, 419, 430, 1090, 148]
+
+from API_functions import get_club_players
+from API_functions import get_age
+
+meta_player_list = []
+
+for club in club_id:
+    meta_player_list.append(get_club_players(club))
+
+print(meta_player_list)
