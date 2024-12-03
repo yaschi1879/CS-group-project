474f55a8c1e7f2a60b75117f240ff41d36dc8880
Larsruefli <lars.ruefli@student.unisg.ch>
2024-12-03 15:22:55 +0100

    ss

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import a_pages.searchbar
-#import a_pages.leaderboard
-#import a_pages.who_am_i
+import a_pages.leaderboard
+import a_pages.who_am_i
 import a_pages.home_page
 
 
@@ -26,7 +26,7 @@ if page == "Home":
     a_pages.home_page.home_page()
 elif page == "Game":
     a_pages.who_am_i.who_am_i()
-#elif page == "Leaderboard":
-    #a_pages.leaderboard.stats()
+elif page == "Leaderboard":
+    a_pages.leaderboard.stats()
 elif page == "Search":
     a_pages.searchbar.searchbar()
