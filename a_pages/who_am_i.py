b7f76110dcdfbd68204088f66dc4fdeff5e462b8
Larsruefli <lars.ruefli@student.unisg.ch>
2024-12-03 16:27:56 +0100

    d

--- a_pages/who_am_i.py
+++ a_pages/who_am_i.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from c_coding.a_api_functions import get_player_name_user_input
-from c_coding.d_game_initialize import initialize_original_player_lists, generate_player_list, initialize_game_variables
+from c_coding.d_game_initialize import initialize_original_player_list, generate_player_list, initialize_game_variables
 from c_coding.f_game_logic import play_game
 from b_support_pages.sp_who_am_i import handle_question_selection
 
