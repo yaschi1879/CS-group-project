bd72b5ed475945706c9c5a3c6276a9dba5bc949f
Yannick <yannick03schmid@gmail.com>
2024-12-04 09:33:28 +0100

    s

--- a_pages/who_am_i.py
+++ a_pages/who_am_i.py
@@ -1,12 +1,7 @@
 import streamlit as st
-from c_coding.a_api_functions import get_player_name_user_input
-from c_coding.d_game_initialize import initialize_original_player_list, generate_player_list, initialize_game_variables
-from c_coding.f_game_logic import play_game
-from b_game_logic.a_questions import handle_question_selection
 
 # Code Lars hier eingefügt!!!
 def who_am_i():
-    import streamlit as st
     st.write("yes")
 
 
