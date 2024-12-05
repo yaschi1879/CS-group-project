e6492f5edcd58b90d014fe07edcc40dcf0b582b7
Yannick <yannick03schmid@gmail.com>
2024-12-05 13:11:48 +0100

    d

--- c_coding/a_api_functions.py
+++ c_coding/a_api_functions.py
@@ -57,6 +57,7 @@ def get_player_name_user_input(player_name):
     if not players:
         return ["n.a.", "n.a."]
     else:
+        print("not correct")
         return [players[0]["id"], players[0]["name"]]
 # item 0 gibt die korrekte player id zurück
 # item 1 gibt den korrekten player namen zurück
