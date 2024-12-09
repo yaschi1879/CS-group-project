2215343f57a90ce2b1d9f06a128b0894f135447e
mathieu-jbn <mathieu.jobin@student.unisg.ch>
2024-12-09 15:45:12 +0100

    ee

--- a_pages/home_page.py
+++ a_pages/home_page.py
@@ -32,7 +32,10 @@ def home_page():
 
                 Think you know football? Prove it in the ultimate online guessing game for fans who live and breathe the beautiful game!  
                 From legendary icons to rising stars across all leagues, eras, and nations, every clue brings you closer to uncovering the mystery player.  
-
+                Here's a hint: all players come from the 40 best clubs across the top 5 leagues – only the elite make the cut!
+                But that’s not all – we’ve implemented a Machine Learning system that predicts the market value of each player 
+                for the next 3 years.
+                 
                 **Can you guess who it is with fewer questions and rack up the most points?**  
                 Challenge yourself, take on your friends, and rise to the top of the leaderboard.  
 
